e85cb72d582f858be32f85a29895af56b6217fd1
mortezasojodi <46606612+mortezasojodi@users.noreply.github.com>
2023-12-22 22:11:49 +0330

    Update requests.dart

--- lib/data/api/requests.dart
+++ lib/data/api/requests.dart
@@ -9,21 +9,7 @@ class DioHelper {
   static Dio getDio() {
     if (null == dio) {
       dio = Dio();
-      // ..interceptors.add(InterceptorsWrapper(onError: (error, e) async {
-      //   // if (error.response?.statusCode == 403 ||
-      //   //     error.response?.statusCode == 401) {}
-      //   var t = e;
-      //   var ff = error;
-      //   VzToast("در روند عملیات خطایی رخ داد",
-      //       colors: Colors.red, textColor: Colors.white);
-      // }));
 
-      // (dio!.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
-      //     (HttpClient client) {
-      //   client.badCertificateCallback =
-      //       (X509Certificate cert, String host, int port) => true;
-      //   return client;
-      // };
     }
     return dio!;
   }
@@ -91,29 +77,6 @@ class Repository {
   }
 }
 
-// class Repository2 {
-//   Future<Response?> MsaPost(u,
-//       {Map<String, dynamic>? p,
-//       Object? body,
-//       Map<String, dynamic>? header}) async {
-//     var response = await DioHelper.getDio().post("https://estadium.app" + u,
-//         data: body,
-//         options: Options(
-//           headers: header,
-//         ));
-//     return response;
-//   }
-
-//   Future<Response?> MsaGet(u,
-//       {Map<String, dynamic>? p, Object? body, header}) async {
-//     var response = await DioHelper.getDio().get("https://estadium.app" + u,
-//         queryParameters: p,
-//         options: Options(
-//           headers: header,
-//         ));
-//     return response;
-//   }
-// }
 
 class RepositoryNoServer {
   Future<Response?> MsaPost(u,
@@ -139,13 +102,4 @@ class RepositoryNoServer {
   }
 }
 
-// class VzRepository {
-//   static Repository? rep;
 
-//   static Repository getRepository() {
-//     if (null == rep) {
-//       rep = Repository();
-//     }
-//     return rep!;
-//   }
-// }
